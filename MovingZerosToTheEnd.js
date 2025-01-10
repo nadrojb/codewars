dc6b395b09ae502e8be0acd75d99dfaa7c517f49
nadrojb <jordanbrewer1994@gmail.com>
2025-01-10 10:58:58 +0000

    working on moving items to end of array

--- MovingZerosToTheEnd.js
+++ MovingZerosToTheEnd.js
@@ -1,8 +1,14 @@
-
 //TASK
 //Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
 
-
 function moveZeros(arr) {
-    return
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 0) {
+      arr.splice(i, i);
+      arr.push(0);
+    }
   }
+  return arr;
+}
+
+console.log(moveZeros([false, 1, 0, 1, 2, 0, 1, 3, "a"]));
